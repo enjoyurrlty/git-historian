8e7b97307815de2139296bcd26a846deb2f8e5d0
Artur Khafizov <ukkc42@ya.ru>
2022-08-27 02:42:58 +0200

    Cleanup commit

--- src/components/Commit.tsx
+++ src/components/Commit.tsx
@@ -9,8 +9,8 @@ type CommitProps = {
 function Commit({ message, date, author }: CommitProps) {
   return (
     <li className='bg-white dark:bg-zinc-700 shadow-md p-4 mb-4 min-w-30 rounded text-left'>
-      <p className='text-2xl font-semibold'>
-        <a href='#'>{message}</a>
+      <p className='text-1xl font-semibold whitespace-pre-line mb-4'>
+        {message}
       </p>
       <div>
         <span>{formatDateToRelative(date)}</span>
